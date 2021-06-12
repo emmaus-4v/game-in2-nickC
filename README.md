19caad1e474680698b81991127b222fe56c4d239
Nick2945 <112326@emmauscollege.nl>
2021-06-12 22:07:56 +0000

    Final stage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Deze game is gebaseerd op het template voor 4HV van het Emmauscollege Rotterdam
 
-## Mijn spel: *naam van het spel*
+## Mijn spel: *osu rx!*
 Gemaakt door:
 - *Nick Cheung *
 
@@ -8,22 +8,11 @@ Gemaakt door:
 
 Basis: uitleg, spelen, afgaan en punten
 - [x] maak index.html, style.css en script.js met canvas
-- [ ] teken speler
-- [ ] beweeg speler
-- [ ] ... *(vul zelf aan)*
-- [ ] punten op scherm zetten
-- [ ] punten kunnen scoren
-- [ ] uitlegscherm
-
-Uitbreiding: *bedenk iets, bijvoorbeeld meer tegenstanders*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-
-Uitbreiding: *bedenk iets, bijvoorbeeld bewegende achtergrond*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
-- [ ] *later uitwerken*
+- [x] teken cirkel
+- [x] punten op scherm zetten
+- [x] punten kunnen scoren
+- [x] levels maken
+- [x] eindscherm
 
 ## Credits
 - Game template van het Emmauscollege Rotterdam https://github.com/emmauscollege/4HV-game-template
--- script.js
+++ script.js
@@ -17,174 +17,101 @@
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
 
-const UITLEG = 0;
-const SPELEN = 1;
-const GAMEOVER = 2;
-var spelStatus = SPELEN;
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+var score = 0;
+var gameState = "L1";
 
-var spelerX = 200; // x-positie van speler
-var spelerY = 100; // y-positie van speler
-
-var kogelX = 0;    // x-positie van kogel
-var kogelY = 0;    // y-positie van kogel
-
-var vijandX = 0;   // x-positie van vijand
-var vijandY = 0;   // y-positie van vijand
-
-var score = 0; // aantal behaalde punten
-
-
-
-
-
-/* ********************************************* */
-/*      functies die je gebruikt in je game      */
-/* ********************************************* */
-
-
-/**
- * Tekent het speelveld
- */
-var tekenVeld = function () {
-  fill("purple");
-  rect(20, 20, width - 2 * 20, height - 2 * 20);
-};
-
-
-/**
- * Tekent de vijand
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenVijand = function(x, y) {
-    
-
-};
-
-
-/**
- * Tekent de kogel of de bal
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenKogel = function(x, y) {
-
-
-
-};
-
-
-/**
- * Tekent de speler
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenSpeler = function(x, y) {
-  fill(255, 255, 255);
-  ellipse(x, y, 50, 50);
-};
-
-
-/**
- * Updatet globale variabelen met positie van vijand of tegenspeler
- */
-var beweegVijand = function() {
-    
-};
-
-
-/**
- * Updatet globale variabelen met positie van kogel of bal
- */
-var beweegKogel = function() {
-
-};
-
-
-/**
- * Kijkt wat de toetsen/muis etc zijn.
- * Updatet globale variabele spelerX en spelerY
- */
-var beweegSpeler = function() {
-
-};
-
-
-/**
- * Zoekt uit of de vijand is geraakt
- * @returns {boolean} true als vijand is geraakt
- */
-var checkVijandGeraakt = function() {
-
-  return false;
-};
-
-
-/**
- * Zoekt uit of de speler is geraakt
- * bijvoorbeeld door botsing met vijand
- * @returns {boolean} true als speler is geraakt
- */
-var checkSpelerGeraakt = function() {
-    
-  return false;
-};
-
-
-/**
- * Zoekt uit of het spel is afgelopen
- * @returns {boolean} true als het spel is afgelopen
- */
-var checkGameOver = function() {
-    
-  return false;
-};
-
-
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
 function setup() {
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  createCanvas(1280, 720);
-
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
-}
+  createCanvas(600, 600);
+  textAlign(CENTER);
+  textSize(20);
+} // end of setup  ================================
 
 
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
 function draw() {
-  switch (spelStatus) {
-    case SPELEN:
-      beweegVijand();
-      beweegKogel();
-      beweegSpeler();
-      
-      if (checkVijandGeraakt()) {
-        // punten erbij
-        // nieuwe vijand maken
-      }
-      
-      if (checkSpelerGeraakt()) {
-        // leven eraf of gezondheid verlagen
-        // eventueel: nieuwe speler maken
-      }
-
-      tekenVeld();
-      tekenVijand(vijandX, vijandY);
-      tekenKogel(kogelX, kogelY);
-      tekenSpeler(spelerX, spelerY);
-
-      if (checkGameOver()) {
-        spelStatus = GAMEOVER;
-      }
-      break;
+  background(220);
+  var currentTime = floor(millis()/1000);
+  
+  if(gameState == "L1"){
+  	levelOne();
   }
-}
+  if(gameState == "L2"){
+   	levelTwo(); 
+  }
+  if(gameState == "L3"){
+   	levelThree(); 
+  }
+  if(gameState == "L4"){
+    levelComplete();
+  }
+  
+  text(("Score: " + score),width/2,40);
+  
+  text('time: ' + currentTime, 200, 40);
+} // end of draw ===================================
+
+function levelOne(){
+  text("Level 1", width/2,height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+   	ballx = random(width);
+  	bally = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 15){
+    gameState = "L2";
+  }
+  
+  ellipse(ballx,bally, ballSize, ballSize);
+  line(ballx,bally,mouseX,mouseY);
+  
+} // end of level 1  ===============================
+
+function levelTwo(){
+  background(200,100,0);
+  text("Level 2!", width/2,height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+   	ballx = random(width);
+  	bally = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 25){
+   gameState = "L3";
+  }
+  //line(ballx,bally,mouseX,mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+} // end of level 2  ===============================
+
+function levelThree(){
+  background(0,100,200);
+  text("Level 3!", width/2,height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+   	ballx = random(width);
+  	bally = random(height);
+    score = score + 1;
+    ballSize = ballSize - 3;
+  }
+  
+  if(score>= 35){
+      gameState = "L4"; 
+  }
+  //line(ballx,bally,mouseX,mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+} // end of level 3  ===============================
+
+function levelComplete(){
+    background(0,200,0);
+    fill(0,200,0);
+    noStroke();
+    rect(200,40,100,80);
+    text("Congratulations!", width/2, height-20);
+    
+  }
